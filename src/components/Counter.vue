536ffe920424785ef052ed2e72377bc0fbd37694
Junsuk Oh <a811219@gmail.com>
2019-04-15 22:19:40 +0900

    카운터 예제

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="counter">
+    <h1>{{ count }}</h1>
+    <br>
+    <button v-on:click="addCounter">+</button>
+    <br>
+    <button v-on:click="subCounter">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Counter',
+    data () {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      addCounter: function () {
+        this.count++
+      },
+      subCounter: function () {
+        this.count--
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
