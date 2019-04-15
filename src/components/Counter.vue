501352fea645c8f560e4f79238923eebcc377ba9
Junsuk Oh <a811219@gmail.com>
2019-04-15 22:29:53 +0900

    bootstrap 적용

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,9 +2,9 @@
   <div id="counter">
     <h1>{{ count }}</h1>
     <br>
-    <button v-on:click="addCounter">+</button>
-    <br>
-    <button v-on:click="subCounter">-</button>
+    <b-button variant="danger" v-on:click="addCounter">+</b-button>
+    &nbsp;
+    <b-button variant="success" v-on:click="subCounter">-</b-button>
   </div>
 </template>
 
