723a161545f1c02c89ea01829dfe4ddb8588eaf1
Junsuk Oh <a811219@gmail.com>
2019-04-16 21:15:32 +0900

    영화 검색 기능

--- src/components/MovieUpcoming.vue
+++ src/components/MovieUpcoming.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     최신 영화 목록
+    <input v-model="query" v-on:change="search">
     <br>
-    <article v-for="(movie, idx) in movies" :key="idx">
+    <article v-for="(movie, idx) in filteredMovies" :key="idx">
       <img v-bind:src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" width="100" height="100" alt=""/>
       <h1>{{movie.title}}</h1>
       {{movie}}
@@ -17,16 +18,21 @@
     name: 'MovieUpcoming',
     data () {
       return {
-        movies: []
+        query: null,
+        movies: [],
+        filteredMovies: []
       }
     },
     methods: {
       getUpcoming: function () {
         axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1')
           .then(value => {
-            console.log(value.data.results)
             this.movies = value.data.results
+            this.filteredMovies = this.movies
           })
+      },
+      search: function () {
+        this.filteredMovies = this.movies.filter(movie => movie.title.toLowerCase().includes(this.query))
       }
     },
     created () {
